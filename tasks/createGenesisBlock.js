7263e2af87b32ee2289172db0a520e48e215412a
Giovanni Silvestri <gsit80@gmail.com>
2018-03-23 12:50:55 +0100

    MaiNET: preparing

--- tasks/createGenesisBlock.js
+++ tasks/createGenesisBlock.js
@@ -11,9 +11,9 @@ var networks = require('../networks.json');
 
 // network name that SHOULD already be preconfigured in ../networks.json
 var network_name = "ripa";
-if(!networks[network_name]){
-  console.log("WARNING: no configuration found in networks.json for '"+network_name+"'. Defaulting to 'devnet'");
-  network_name = "devnet";
+if (!networks[network_name]) {
+    console.log("WARNING: no configuration found in networks.json for '" + network_name + "'. Defaulting to 'devnet'");
+    network_name = "devnet";
 }
 
 // directory to export passphrases of premine account and genesis delegates. Should exist
@@ -30,26 +30,26 @@ var config_version = '0.0.1';
 
 // ips of your nodes in your network
 var seed_peers = [
-        {
+    {
         ip: "127.0.0.1",
         port: 5500
-      },{
+    }, {
         ip: "127.0.0.2",
         port: 5500
-      },{
+    }, {
         ip: "127.0.0.3",
         port: 5500
-      },{
+    }, {
         ip: "127.0.0.4",
         port: 5500
-      },{
+    }, {
         ip: "127.0.0.5",
         port: 5500
-      }
+    }
 ];
 
 // default db named
-var db_name = "ripa_" + network_name;
+var db_name = "ripa_" + networks[network_name].name;
 
 // optional premined accounts. Example :
 // [
@@ -58,11 +58,11 @@ var db_name = "ripa_" + network_name;
 // ]
 // total in satoshi
 var genesisAccounts = [];
-if(fs.existsSync('./private/genesis.'+network_name+'.accounts.json')) {
-    var genesisAccounts = JSON.parse(fs.readFileSync('./private/genesis.'+network_name+'.accounts.json'));
+if (fs.existsSync('./private/genesis.' + networks[network_name].name + '.accounts.json')) {
+    var genesisAccounts = JSON.parse(fs.readFileSync('./private/genesis.' + networks[network_name].name + '.accounts.json'));
 }
 else {
-  console.log("WARNING: no premined accounts found ('./private/genesis."+network_name+".accounts.json').");
+    console.log("WARNING: no premined accounts found ('./private/genesis." + networks[network_name].name + ".accounts.json').");
 }
 
 // Total of premined token in satoshi. The premined accounts will be substracted to this
@@ -92,7 +92,7 @@ var config = {
         ]
     },
     api: {
-        mount:true,
+        mount: true,
         access: {
             whiteList: []
         },
@@ -106,7 +106,7 @@ var config = {
         }
     },
     peers: {
-        minimumNetworkReach:1,
+        minimumNetworkReach: 1,
         list: seed_peers,
         blackList: [],
         options: {
@@ -147,140 +147,140 @@ var config = {
 };
 // general functions
 makeKeypair = function (seed) {
-	return ripajs.crypto.getKeys(seed, networks[config.network]);
+    return ripajs.crypto.getKeys(seed, networks[config.network]);
 };
 
 sign = function (block, keypair) {
-	var hash = getHash(block);
-	return keypair.sign(hash).toDER().toString("hex");
+    var hash = getHash(block);
+    return keypair.sign(hash).toDER().toString("hex");
 };
 
 
 getId = function (block) {
-	var hash = crypto.createHash('sha256').update(getBytes(block)).digest();
-	var temp = new Buffer(8);
-	for (var i = 0; i < 8; i++) {
-		temp[i] = hash[7 - i];
-	}
+    var hash = crypto.createHash('sha256').update(getBytes(block)).digest();
+    var temp = new Buffer(8);
+    for (var i = 0; i < 8; i++) {
+        temp[i] = hash[7 - i];
+    }
 
-	var id = bignum.fromBuffer(temp).toString();
-	return id;
+    var id = bignum.fromBuffer(temp).toString();
+    return id;
 };
 
 getHash = function (block) {
-	return crypto.createHash('sha256').update(getBytes(block)).digest();
+    return crypto.createHash('sha256').update(getBytes(block)).digest();
 };
 
 
 getBytes = function (block) {
-	var size = 4 + 4 + 4 + 8 + 4 + 4 + 8 + 8 + 4 + 4 + 4 + 32 + 32 + 64;
-	var b, i;
+    var size = 4 + 4 + 4 + 8 + 4 + 4 + 8 + 8 + 4 + 4 + 4 + 32 + 32 + 64;
+    var b, i;
 
-	try {
-		var bb = new ByteBuffer(size, true);
-		bb.writeInt(block.version);
-		bb.writeInt(block.timestamp);
-    bb.writeInt(block.height);
+    try {
+        var bb = new ByteBuffer(size, true);
+        bb.writeInt(block.version);
+        bb.writeInt(block.timestamp);
+        bb.writeInt(block.height);
 
-		if (block.previousBlock) {
-			var pb = bignum(block.previousBlock).toBuffer({size: '8'});
+        if (block.previousBlock) {
+            var pb = bignum(block.previousBlock).toBuffer({ size: '8' });
 
-			for (i = 0; i < 8; i++) {
-				bb.writeByte(pb[i]);
-			}
-		} else {
-			for (i = 0; i < 8; i++) {
-				bb.writeByte(0);
-			}
-		}
+            for (i = 0; i < 8; i++) {
+                bb.writeByte(pb[i]);
+            }
+        } else {
+            for (i = 0; i < 8; i++) {
+                bb.writeByte(0);
+            }
+        }
 
-		bb.writeInt(block.numberOfTransactions);
-		bb.writeLong(block.totalAmount);
-		bb.writeLong(block.totalFee);
-		bb.writeLong(block.reward);
+        bb.writeInt(block.numberOfTransactions);
+        bb.writeLong(block.totalAmount);
+        bb.writeLong(block.totalFee);
+        bb.writeLong(block.reward);
 
-		bb.writeInt(block.payloadLength);
+        bb.writeInt(block.payloadLength);
 
-		var payloadHashBuffer = new Buffer(block.payloadHash, 'hex');
-		for (i = 0; i < payloadHashBuffer.length; i++) {
-			bb.writeByte(payloadHashBuffer[i]);
-		}
+        var payloadHashBuffer = new Buffer(block.payloadHash, 'hex');
+        for (i = 0; i < payloadHashBuffer.length; i++) {
+            bb.writeByte(payloadHashBuffer[i]);
+        }
 
-		var generatorPublicKeyBuffer = new Buffer(block.generatorPublicKey, 'hex');
-		for (i = 0; i < generatorPublicKeyBuffer.length; i++) {
-			bb.writeByte(generatorPublicKeyBuffer[i]);
-		}
+        var generatorPublicKeyBuffer = new Buffer(block.generatorPublicKey, 'hex');
+        for (i = 0; i < generatorPublicKeyBuffer.length; i++) {
+            bb.writeByte(generatorPublicKeyBuffer[i]);
+        }
 
-		if (block.blockSignature) {
-			var blockSignatureBuffer = new Buffer(block.blockSignature, 'hex');
-			for (i = 0; i < blockSignatureBuffer.length; i++) {
-				bb.writeByte(blockSignatureBuffer[i]);
-			}
-		}
+        if (block.blockSignature) {
+            var blockSignatureBuffer = new Buffer(block.blockSignature, 'hex');
+            for (i = 0; i < blockSignatureBuffer.length; i++) {
+                bb.writeByte(blockSignatureBuffer[i]);
+            }
+        }
 
-		bb.flip();
-		b = bb.toBuffer();
-	} catch (e) {
-		throw e;
-	}
+        bb.flip();
+        b = bb.toBuffer();
+    } catch (e) {
+        throw e;
+    }
 
-	return b;
+    return b;
 };
 
 create = function (data) {
-	var transactions = data.transactions.sort(function compare(a, b) {
-		if (a.type < b.type) { return -1; }
-		if (a.type > b.type) { return 1; }
-		if (a.amount < b.amount) { return -1; }
-		if (a.amount > b.amount) { return 1; }
-		return 0;
-	});
+    var transactions = data.transactions.sort(function compare(a, b) {
+        if (a.type < b.type) { return -1; }
+        if (a.type > b.type) { return 1; }
+        if (a.amount < b.amount) { return -1; }
+        if (a.amount > b.amount) { return 1; }
+        return 0;
+    });
 
-	var nextHeight = 1;
+    var nextHeight = 1;
 
-	var reward = 0,
-	    totalFee = 0, totalAmount = 0, size = 0;
+    var reward = 0,
+        totalFee = 0, totalAmount = 0, size = 0;
 
-	var blockTransactions = [];
-	var payloadHash = crypto.createHash('sha256');
+    var blockTransactions = [];
+    var payloadHash = crypto.createHash('sha256');
 
-	for (var i = 0; i < transactions.length; i++) {
-		var transaction = transactions[i];
-		var bytes = ripajs.crypto.getBytes(transaction);
+    for (var i = 0; i < transactions.length; i++) {
+        var transaction = transactions[i];
+        var bytes = ripajs.crypto.getBytes(transaction);
 
-		size += bytes.length;
+        size += bytes.length;
 
-		totalFee += transaction.fee;
-		totalAmount += transaction.amount;
+        totalFee += transaction.fee;
+        totalAmount += transaction.amount;
 
-		blockTransactions.push(transaction);
-		payloadHash.update(bytes);
-	}
+        blockTransactions.push(transaction);
+        payloadHash.update(bytes);
+    }
 
-	var block = {
-		version: 0,
-		totalAmount: totalAmount,
-		totalFee: totalFee,
-		reward: reward,
-		payloadHash: payloadHash.digest().toString('hex'),
-		timestamp: data.timestamp,
-		numberOfTransactions: blockTransactions.length,
-		payloadLength: size,
-		previousBlock: null,
-		generatorPublicKey: data.keypair.publicKey.toString('hex'),
-		transactions: blockTransactions,
-    height:1
-	};
+    var block = {
+        version: 0,
+        totalAmount: totalAmount,
+        totalFee: totalFee,
+        reward: reward,
+        payloadHash: payloadHash.digest().toString('hex'),
+        timestamp: data.timestamp,
+        numberOfTransactions: blockTransactions.length,
+        payloadLength: size,
+        previousBlock: null,
+        generatorPublicKey: data.keypair.publicKey.toString('hex'),
+        transactions: blockTransactions,
+        height: 1
+    };
 
-  block.id=getId(block);
+    block.id = getId(block);
 
-	try {
-		block.blockSignature = sign(block, data.keypair);
-	} catch (e) {
-		throw e;
-	}
+    try {
+        block.blockSignature = sign(block, data.keypair);
+    } catch (e) {
+        throw e;
+    }
 
-	return block;
+    return block;
 }
 
 
@@ -292,12 +292,12 @@ var remainingfund = {};
 ripajs.crypto.setNetworkVersion(networks[network_name].pubKeyHash);
 
 var genesis = {
-  passphrase: bip39.generateMnemonic(),
-  balance: totalpremine
+    passphrase: bip39.generateMnemonic(),
+    balance: totalpremine
 };
 
 var premine = {
-  passphrase: bip39.generateMnemonic()
+    passphrase: bip39.generateMnemonic()
 };
 
 premine.publicKey = ripajs.crypto.getKeys(premine.passphrase).publicKey;
@@ -307,43 +307,43 @@ genesis.publicKey = ripajs.crypto.getKeys(genesis.passphrase).publicKey;
 genesis.address = ripajs.crypto.getAddress(genesis.publicKey, networks[config.network].pubKeyHash);
 
 // creation of delegates
-for(var i=1; i<52; i++){
-  var delegate = {
-    'passphrase': bip39.generateMnemonic(),
-    'username': "genesis_"+i
-  };
+for (var i = 1; i < 52; i++) {
+    var delegate = {
+        'passphrase': bip39.generateMnemonic(),
+        'username': "genesis_" + i
+    };
 
-	delegate.publicKey = ripajs.crypto.getKeys(delegate.passphrase).publicKey;
-	delegate.address = ripajs.crypto.getAddress(delegate.publicKey, networks[config.network].pubKeyHash);
+    delegate.publicKey = ripajs.crypto.getKeys(delegate.passphrase).publicKey;
+    delegate.address = ripajs.crypto.getAddress(delegate.publicKey, networks[config.network].pubKeyHash);
 
-	// create delegate
-  var createDelegateTx = ripajs.delegate.createDelegate(delegate.passphrase, delegate.username);
-  createDelegateTx.fee = 0;
-  createDelegateTx.timestamp = 0;
-  createDelegateTx.senderId = delegate.address;
-  createDelegateTx.signature = ripajs.crypto.sign(createDelegateTx,ripajs.crypto.getKeys(delegate.passphrase));
-  createDelegateTx.id = ripajs.crypto.getId(createDelegateTx);
+    // create delegate
+    var createDelegateTx = ripajs.delegate.createDelegate(delegate.passphrase, delegate.username);
+    createDelegateTx.fee = 0;
+    createDelegateTx.timestamp = 0;
+    createDelegateTx.senderId = delegate.address;
+    createDelegateTx.signature = ripajs.crypto.sign(createDelegateTx, ripajs.crypto.getKeys(delegate.passphrase));
+    createDelegateTx.id = ripajs.crypto.getId(createDelegateTx);
 
-  transactions.push(createDelegateTx);
+    transactions.push(createDelegateTx);
 
-  delegates.push(delegate);
+    delegates.push(delegate);
 }
 
 var total = 0;
 
-for(var i=0; i < genesisAccounts.length; i++){
-  var account = genesisAccounts[i];
-  total += account.total;
+for (var i = 0; i < genesisAccounts.length; i++) {
+    var account = genesisAccounts[i];
+    total += account.total;
 
-	//send ripa to account
-	var premineTx = ripajs.transaction.createTransaction(account.address, account.total, null, premine.passphrase);
+    //send ripa to account
+    var premineTx = ripajs.transaction.createTransaction(account.address, account.total, null, premine.passphrase);
 
-	premineTx.fee = 0;
-	premineTx.timestamp = 0;
-	premineTx.senderId = premine.address;
-	premineTx.signature = ripajs.crypto.sign(premineTx,ripajs.crypto.getKeys(premine.passphrase));
-	premineTx.id = ripajs.crypto.getId(premineTx);
-	transactions.push(premineTx);
+    premineTx.fee = 0;
+    premineTx.timestamp = 0;
+    premineTx.senderId = premine.address;
+    premineTx.signature = ripajs.crypto.sign(premineTx, ripajs.crypto.getKeys(premine.passphrase));
+    premineTx.id = ripajs.crypto.getId(premineTx);
+    transactions.push(premineTx);
 
 }
 
@@ -354,41 +354,41 @@ var preminefund = ripajs.transaction.createTransaction(genesis.address, remainin
 preminefund.fee = 0;
 preminefund.timestamp = 0;
 preminefund.senderId = premine.address;
-preminefund.signature = ripajs.crypto.sign(preminefund,ripajs.crypto.getKeys(premine.passphrase));
+preminefund.signature = ripajs.crypto.sign(preminefund, ripajs.crypto.getKeys(premine.passphrase));
 preminefund.id = ripajs.crypto.getId(preminefund);
 transactions.push(preminefund);
 
 var genesisBlock = create({
-  keypair: ripajs.crypto.getKeys(genesis.passphrase, networks[config.network]),
-  transactions:transactions,
-  timestamp:0
+    keypair: ripajs.crypto.getKeys(genesis.passphrase, networks[config.network]),
+    transactions: transactions,
+    timestamp: 0
 });
 
 config.nethash = genesisBlock.payloadHash;
 
-fs.writeFile(output_dir+"/genesisBlock."+config.network+".json",JSON.stringify(genesisBlock, null, 2));
-fs.writeFile(output_dir+"/config."+config.network+".json",JSON.stringify(config, null, 2));
+fs.writeFile(output_dir + "/genesisBlock." + config.network + ".json", JSON.stringify(genesisBlock, null, 2));
+fs.writeFile(output_dir + "/config." + config.network + ".json", JSON.stringify(config, null, 2));
 
 // add delegates passphrases in config for testing on one single node
-for(var i=0;i<51;i++){
-	config.forging.secret.push(delegates[i].passphrase);
+for (var i = 0; i < 51; i++) {
+    config.forging.secret.push(delegates[i].passphrase);
 }
-fs.writeFile(private_dir+"/config."+config.network+".autoforging.json", JSON.stringify(config, null, 2));
+fs.writeFile(private_dir + "/config." + config.network + ".autoforging.json", JSON.stringify(config, null, 2));
 
 var forging = [];
-seed_peers.forEach(function(seed){
-    forging.push({secret:[]});
+seed_peers.forEach(function (seed) {
+    forging.push({ secret: [] });
 });
 // split all delegates accross all seed_peers
-for(var i=0;i<51;i++){
-  var seed_index = i % seed_peers.length;
-  forging[seed_index].secret.push(delegates[i].passphrase);
+for (var i = 0; i < 51; i++) {
+    var seed_index = i % seed_peers.length;
+    forging[seed_index].secret.push(delegates[i].passphrase);
 }
 
-seed_peers.forEach(function(peer,index){
-  config.forging.secret = forging[index];
-  fs.writeFile(private_dir+"/config."+config.network+"."+peer.ip+".json", JSON.stringify(config, null, 2));
+seed_peers.forEach(function (peer, index) {
+    config.forging.secret = forging[index];
+    fs.writeFile(private_dir + "/config." + config.network + "." + peer.ip + ".json", JSON.stringify(config, null, 2));
 });
 
-fs.writeFile(private_dir+"/delegatesPassphrases."+config.network+".json", JSON.stringify(delegates, null, 2));
-fs.writeFile(private_dir+"/genesisPassphrase."+config.network+".json", JSON.stringify(genesis, null, 2));
+fs.writeFile(private_dir + "/delegatesPassphrases." + config.network + ".json", JSON.stringify(delegates, null, 2));
+fs.writeFile(private_dir + "/genesisPassphrase." + config.network + ".json", JSON.stringify(genesis, null, 2));
